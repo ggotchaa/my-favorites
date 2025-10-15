48ee82e843506891b9c26b25418b3441bb28c34c
Nursultan <nurbekenov.nb@gmail.com>
2025-10-15 21:33:24 +0500

    asd

--- main-project/src/environments/env.prod.ts
+++ main-project/src/environments/env.prod.ts
@@ -1,17 +1,17 @@
-const baseApiUrl = 'https://tcoacd-acd-dev-backend.azurewebsites.net';
+const baseApiUrl = 'https://tcobid-backend-dev.azurewebsites.net/api';
 
 export const environment = {
   production: true,
   apiBaseUrl: `${baseApiUrl}/api`,
   baseApiURL: baseApiUrl,
-  baseURLScope: 'https://tcoacd-dev.azure.chevron.com/Read',
+  baseURLScope: 'https://tcobid-dev.azure.chevron.com/Read',
   redirectUri:
     typeof window !== 'undefined'
       ? `${window.location.origin}${window.location.pathname}`
       : '',
   instance: 'https://login.microsoftonline.com/',
   tenantId: 'fd799da1-bfc1-4234-a91c-72b3a1cb9e26',
-  clientId: '688a6d31-fc3c-4353-867d-95cdd927825c',
+  clientId: '58a40fb4-a4a5-4a4d-b75b-1f9a312d9480',
   oidcScopes: ['Read', 'user_impersonation', 'Write'],
   graphScopes: ['User.Read', 'User.Read.All'],
   autoSignIn: true,
