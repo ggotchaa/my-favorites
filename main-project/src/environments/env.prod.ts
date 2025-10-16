58fb9f1691ff0a8ff992f24d58b6814e321ab595
Bekenov, Nursultan Norsec Delta Projects LLP <Nursultan.Bekenov@tengizchevroil.com>
2025-10-16 15:45:44 +0500

    asd

--- main-project/src/environments/env.prod.ts
+++ main-project/src/environments/env.prod.ts
@@ -2,9 +2,9 @@ const baseApiUrl = 'https://tcobid-backend-dev.azurewebsites.net/api';
 
 export const environment = {
   production: true,
-  apiBaseUrl: `${baseApiUrl}/api`,
+  apiBaseUrl: `${baseApiUrl}`,
   baseApiURL: baseApiUrl,
-  baseURLScope: 'https://tcobid-dev.azure.chevron.com/Read',
+  baseURLScope: 'https://tcobid.azure.chevron.com/',
   redirectUri:
     typeof window !== 'undefined'
       ? `${window.location.origin}${window.location.pathname}`
@@ -12,7 +12,7 @@ export const environment = {
   instance: 'https://login.microsoftonline.com/',
   tenantId: 'fd799da1-bfc1-4234-a91c-72b3a1cb9e26',
   clientId: '58a40fb4-a4a5-4a4d-b75b-1f9a312d9480',
-  oidcScopes: ['Read', 'user_impersonation', 'Write'],
+  oidcScopes: ['User.Read', 'user_impersonation', 'User.Write', 'https://tcobid-test.azure.chevron.com/access_as_user'],
   graphScopes: ['User.Read', 'User.Read.All'],
   autoSignIn: true,
   popupForLogin: false,
